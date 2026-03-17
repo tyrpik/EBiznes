8cd3d3ef9d8bc6a3e069d3fe564c0fbfae123c4b
tyrpik <154254042+tyrpik@users.noreply.github.com>
2026-03-17 19:44:57 +0100

    Ostateczne czyszczenie repozytorium

--- task_4_5/src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Java version: " + System.getProperty("java.version"));
-    }
-}
