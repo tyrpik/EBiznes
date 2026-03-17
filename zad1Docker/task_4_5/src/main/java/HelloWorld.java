8e44442b35bcd6ade7ab908231c333b8331eec02
tyrpik <154254042+tyrpik@users.noreply.github.com>
2026-03-17 19:37:23 +0100

    Porządki: przeniesienie zadań 4.0-5.0 do właściwego podfolderu

---
+++ zad1Docker/task_4_5/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+}
